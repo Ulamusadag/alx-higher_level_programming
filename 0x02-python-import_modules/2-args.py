9a0b925f1c96120337cace16f8ab7fe15ba2536b
Ulamusadag <ulamusadag@gmail.com>
2023-12-12 00:01:17 -0800

    2th

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+if __name__ == "__main__":
+    num = len(sys.argv)
+    if num == 1:
+        print("{} argument:".format(num))
+        print("{}: {}".format(num, sys.argv[1]))
+    elif num == 0:
+        print("{} arguments.".format(num))
+    else:
+        for i in sys.argv[1:]:
+            j = 1
+            print("{} arguments:".format(num))
+            print("{}: {}".format(j, i))
+            j = j + 1
