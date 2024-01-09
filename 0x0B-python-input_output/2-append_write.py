18b493d9a560b282cc8ed7d6247358fade9d481a
Ulamusadag <ulamusadag@gmail.com>
2024-01-09 14:45:58 -0800

    2nd

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""function the appends new texxt in already existing files or create a new file"""
+"""function the appends new texxt in already existing"""
 
 
 def write(filename="", text=""):
-    "Return yhe number of the added text
+    """Return yhe number of the added text
 
     Args:
         filename (str): the name of the file
@@ -13,4 +13,3 @@ def write(filename="", text=""):
     """
     with open(filename, a, encoding="utf-8") as f:
         return f.write(text)
-
