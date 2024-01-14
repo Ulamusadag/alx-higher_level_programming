049758fc6c30ee7abbaf632e0314f6adfaa1169c
Ulamusadag <ulamusadag@gmail.com>
2024-01-14 04:13:01 -0800

    2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module description"""
+
+
+def is_same_class(obj, a_class):
+    """ checks of is instance
+
+    Args:
+    obj(any): the object
+    a_class (type): class
+    Returns:
+    return
+    """
+    return isinstance(obj, a_class)
