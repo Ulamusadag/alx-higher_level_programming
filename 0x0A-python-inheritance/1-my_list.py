05b58f388c64c177f6dc80ff0ceecaff5300b3a5
Ulamusadag <ulamusadag@gmail.com>
2024-01-14 03:51:32 -0800

    1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""class description"""
+"""
+contains the MyList class
+"""
 
 
 class MyList(list):
-    """class"""
+    """a subclass of list"""
     def __init__(self):
-        """initialize"""
+        """initializes the object"""
         super().__init__()
 
     def print_sorted(self):
-        """print sorted"""
+        """prints the sorted list"""
         print(sorted(self))
-
 
