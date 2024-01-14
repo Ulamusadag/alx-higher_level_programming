c777fabff0f34641903f86b384c47e144957fc68
Ulamusadag <ulamusadag@gmail.com>
2024-01-14 12:19:09 -0800

    6

---
+++ 0x0B-python-input_output/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/puyhon3
+"""module"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """function"""
+        raise Exception("area() is not implemented")
